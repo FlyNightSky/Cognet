97aeba9597f8c3f0b61658d7eefa389d10bed024
FlyNightSky <118761018+FlyNightSky@users.noreply.github.com>
2023-05-23 15:22:24 -0400

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import { inject } from '@vercel/analytics';
-
-inject();
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), inject()],
 });
