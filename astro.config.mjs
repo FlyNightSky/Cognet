861dc5375291ff09df6861b6ce006e9f5bcdc0f7
FlyNightSky <streamafe@fbi.ac>
2023-05-31 13:00:36 -0400

    fsfdds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-
+import { inject } from '@vercel/analytics';
 import tailwind from "@astrojs/tailwind";
-
+inject();
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
